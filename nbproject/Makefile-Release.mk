d53886a9d3aa81e7e24cc8fdaee8468d68c387cc
Laís Mendes Gonçalves <laismendes0777@gmail.com>
2013-02-17 13:34:21 -0300

    adicionando o projeto do netbeans para testar os testes

---
+++ nbproject/Makefile-Release.mk
@@ -0,0 +1,251 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+GREP=grep
+NM=nm
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc.exe
+CCC=g++.exe
+CXX=g++.exe
+FC=gfortran.exe
+AS=as.exe
+
+# Macros
+CND_PLATFORM=MinGW_1-Windows
+CND_DLIB_EXT=dll
+CND_CONF=Release
+CND_DISTDIR=dist
+CND_BUILDDIR=build
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/arqtexto.o \
+	${OBJECTDIR}/Persistencia.o \
+	${OBJECTDIR}/ErroSave.o \
+	${OBJECTDIR}/Tabuleiro.o \
+	${OBJECTDIR}/Peca.o \
+	${OBJECTDIR}/Casa.o \
+	${OBJECTDIR}/jogo.o
+
+# Test Directory
+TESTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tests
+
+# Test Files
+TESTFILES= \
+	${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/projeto_poo
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=
+CXXFLAGS=
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Assembler Flags
+ASFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/projeto_poo.exe
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/projeto_poo.exe: ${OBJECTFILES}
+	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
+	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/projeto_poo ${OBJECTFILES} ${LDLIBSOPTIONS} 
+
+${OBJECTDIR}/arqtexto.o: arqtexto.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/arqtexto.o arqtexto.cpp
+
+${OBJECTDIR}/Persistencia.o: Persistencia.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/Persistencia.o Persistencia.cpp
+
+${OBJECTDIR}/ErroSave.o: ErroSave.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/ErroSave.o ErroSave.cpp
+
+${OBJECTDIR}/Tabuleiro.o: Tabuleiro.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/Tabuleiro.o Tabuleiro.cpp
+
+${OBJECTDIR}/Peca.o: Peca.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/Peca.o Peca.cpp
+
+${OBJECTDIR}/Casa.o: Casa.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/Casa.o Casa.cpp
+
+${OBJECTDIR}/jogo.o: jogo.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/jogo.o jogo.cpp
+
+# Subprojects
+.build-subprojects:
+
+# Build Test Targets
+.build-tests-conf: .build-conf ${TESTFILES}
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/projeto_poo: ${TESTDIR}/newtestclass.o ${TESTDIR}/newtestrunner.o ${TESTDIR}/testmain.o ${OBJECTFILES:%.o=%_nomain.o}
+	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
+	${LINK.cc}   -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/projeto_poo $^ ${LDLIBSOPTIONS} 
+
+
+${TESTDIR}/newtestclass.o: newtestclass.cpp 
+	${MKDIR} -p ${TESTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${TESTDIR}/newtestclass.o newtestclass.cpp
+
+
+${TESTDIR}/newtestrunner.o: newtestrunner.cpp 
+	${MKDIR} -p ${TESTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${TESTDIR}/newtestrunner.o newtestrunner.cpp
+
+
+${TESTDIR}/testmain.o: testmain.cpp 
+	${MKDIR} -p ${TESTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${TESTDIR}/testmain.o testmain.cpp
+
+
+${OBJECTDIR}/arqtexto_nomain.o: ${OBJECTDIR}/arqtexto.o arqtexto.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/arqtexto.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} $@.d;\
+	    $(COMPILE.cc) -O2 -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/arqtexto_nomain.o arqtexto.cpp;\
+	else  \
+	    ${CP} ${OBJECTDIR}/arqtexto.o ${OBJECTDIR}/arqtexto_nomain.o;\
+	fi
+
+${OBJECTDIR}/Persistencia_nomain.o: ${OBJECTDIR}/Persistencia.o Persistencia.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/Persistencia.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} $@.d;\
+	    $(COMPILE.cc) -O2 -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/Persistencia_nomain.o Persistencia.cpp;\
+	else  \
+	    ${CP} ${OBJECTDIR}/Persistencia.o ${OBJECTDIR}/Persistencia_nomain.o;\
+	fi
+
+${OBJECTDIR}/ErroSave_nomain.o: ${OBJECTDIR}/ErroSave.o ErroSave.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/ErroSave.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} $@.d;\
+	    $(COMPILE.cc) -O2 -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/ErroSave_nomain.o ErroSave.cpp;\
+	else  \
+	    ${CP} ${OBJECTDIR}/ErroSave.o ${OBJECTDIR}/ErroSave_nomain.o;\
+	fi
+
+${OBJECTDIR}/Tabuleiro_nomain.o: ${OBJECTDIR}/Tabuleiro.o Tabuleiro.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/Tabuleiro.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} $@.d;\
+	    $(COMPILE.cc) -O2 -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/Tabuleiro_nomain.o Tabuleiro.cpp;\
+	else  \
+	    ${CP} ${OBJECTDIR}/Tabuleiro.o ${OBJECTDIR}/Tabuleiro_nomain.o;\
+	fi
+
+${OBJECTDIR}/Peca_nomain.o: ${OBJECTDIR}/Peca.o Peca.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/Peca.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} $@.d;\
+	    $(COMPILE.cc) -O2 -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/Peca_nomain.o Peca.cpp;\
+	else  \
+	    ${CP} ${OBJECTDIR}/Peca.o ${OBJECTDIR}/Peca_nomain.o;\
+	fi
+
+${OBJECTDIR}/Casa_nomain.o: ${OBJECTDIR}/Casa.o Casa.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/Casa.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} $@.d;\
+	    $(COMPILE.cc) -O2 -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/Casa_nomain.o Casa.cpp;\
+	else  \
+	    ${CP} ${OBJECTDIR}/Casa.o ${OBJECTDIR}/Casa_nomain.o;\
+	fi
+
+${OBJECTDIR}/jogo_nomain.o: ${OBJECTDIR}/jogo.o jogo.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/jogo.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} $@.d;\
+	    $(COMPILE.cc) -O2 -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/jogo_nomain.o jogo.cpp;\
+	else  \
+	    ${CP} ${OBJECTDIR}/jogo.o ${OBJECTDIR}/jogo_nomain.o;\
+	fi
+
+# Run Test Targets
+.test-conf:
+	@if [ "${TEST}" = "" ]; \
+	then  \
+	    ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/projeto_poo || true; \
+	else  \
+	    ./${TEST} || true; \
+	fi
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
+	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/projeto_poo.exe
+
+# Subprojects
+.clean-subprojects:
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
